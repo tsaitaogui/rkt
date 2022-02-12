92e1118d2f86a1482aa227bed1fb4aa9680e5bee
tsaitaogui <hardluck0410@gmail.com>
2022-02-13 00:35:02 +0800

    a4

---
+++ a4.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+(define binary-to-decimal-cps
+  (lambda (n k)
+    (cond
+      [(null? n) (k 0)]
+      [else (binary-to-decimal-cps
+             (cdr n)
+             (lambda (v)
+              (+ v (* 2 (k (cdr n))))))]
+      )))
+
+
+(define empty-k
+  (lambda ()
+    (lambda (v) v)))
+
+( binary-to-decimal-cps '(1 1 0 1) (empty-k))
