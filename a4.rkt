eb84d294ef00558cb77037e07d358a8178fdc4f8
tsaitaogui <hardluck0410@gmail.com>
2022-02-13 13:51:43 +0800

    a4_2

--- a4.rkt
+++ a4.rkt
@@ -1,5 +1,6 @@
 #lang racket
 
+;1
 (define binary-to-decimal-cps
   (lambda (n k)
     (cond
@@ -15,4 +16,17 @@
   (lambda ()
     (lambda (v) v)))
 
-( binary-to-decimal-cps '(1 1 0 1) (empty-k))
+(binary-to-decimal-cps '() (empty-k))
+(binary-to-decimal-cps '(1) (empty-k))
+(binary-to-decimal-cps '(0 1) (empty-k))
+(binary-to-decimal-cps '(1 1 0 1) (empty-k))
+
+;2
+(define star-cps
+  (lambda (m k)
+    (lambda (n)
+      (k (* m n)))))
+
+((star-cps 2 (empty-k)) 3)
+
+((star-cps ((star-cps 2 (empty-k)) 3) (empty-k)) 5)
