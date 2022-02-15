88389b8491ff7ca27024c7527125eae660f3782d
tsaitaogui <hardluck0410@gmail.com>
2022-02-15 21:13:54 +0800

    add a4.4 1

--- a4.rkt
+++ a4.rkt
@@ -44,3 +44,25 @@
 (times-cps '(1 2 3 4 5) (empty-k))
 
 (times-cps '(1 2 3 0 3) (empty-k))
+
+;4
+(define remv-first-9*-cps
+  (lambda (ls k)
+    (cond
+      [(null? ls) (k '())]
+      [(pair? (car ls))
+       (cond
+         [(equal? (car ls)
+                  (remv-first-9*-cps (car ls) (lambda (v)
+                                                (k v))))
+          (remv-first-9*-cps (cdr ls) (lambda (v)
+                                        (cons (car ls) (k v))))]
+         [else (remv-first-9*-cps (car ls) (lambda (v)
+                                               (cons (k v) (cdr ls)))) ])]
+      [(eqv? (car ls) '9) (k (cdr ls))]
+      [else (remv-first-9*-cps (cdr ls) (lambda (v)
+                                      (cons (car ls) (k v))))])))
+
+(remv-first-9*-cps '((1 2 (3) 9)) (empty-k)) ; TODO
+(remv-first-9*-cps '(9 (9 (9 (9)))) (empty-k))
+(remv-first-9*-cps '(((((9) 9) 9) 9) 9) (empty-k))
